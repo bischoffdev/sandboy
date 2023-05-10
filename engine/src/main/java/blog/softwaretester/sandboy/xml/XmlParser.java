fff48a2b693c6e9806a97b8cdb605cd8f296f7b0
Benjamin Bischoff <benjamin.bischoff@gmx.de>
2023-05-10 13:31:20 +0200

    example xml

--- engine/src/main/java/blog/softwaretester/sandboy/xml/XmlParser.java
+++ engine/src/main/java/blog/softwaretester/sandboy/xml/XmlParser.java
@@ -1,10 +1,12 @@
 package blog.softwaretester.sandboy.xml;
 
 import blog.softwaretester.sandboy.xml.pojo.Report;
+import org.jdom2.Document;
 import org.jdom2.Element;
 import org.jdom2.input.SAXBuilder;
 
 import javax.xml.XMLConstants;
+import java.util.List;
 
 public class XmlParser {
 
@@ -16,7 +18,37 @@ public class XmlParser {
         sax.setProperty(XMLConstants.ACCESS_EXTERNAL_SCHEMA, "");
     }
 
-    public Report parseXmlReport(final Element root) {
+    public Report parseXmlReport(final Document document) {
+        try {
+            Element rootElement = document.getRootElement();
+
+            int testsRun = Integer.parseInt(rootElement.getAttributeValue("tests"));
+            int testsPassed = Integer.parseInt(rootElement.getAttributeValue("passed"));
+            int testsFailed = Integer.parseInt(rootElement.getAttributeValue("failures"));
+
+            System.out.println("Tests run: " + testsRun);
+            System.out.println("Tests passed: " + testsPassed);
+            System.out.println("Tests failed: " + testsFailed);
+
+            // Get the test cases
+            List<Element> testCases = rootElement.getChildren("testcase");
+            for (Element testCase : testCases) {
+                String className = testCase.getAttributeValue("classname");
+                String testName = testCase.getAttributeValue("name");
+                double timeTaken = Double.parseDouble(testCase.getAttributeValue("time"));
+                String status = "passed";
+
+                // Check if the test failed
+                Element failure = testCase.getChild("failure");
+                if (failure != null) {
+                    status = "failed";
+                }
+
+                System.out.println(className + "." + testName + " (" + timeTaken + "s): " + status);
+            }
+        } catch (Exception e) {
+            System.out.println("FAILURE: " + e.getMessage());
+        }
         return new Report();
     }
 }
